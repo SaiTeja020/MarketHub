0655fe5de134138f7a9aeb19c93ec3ec705b61cf
SaiTeja020 <sakamsaiteja@gmail.com>
2025-11-25 16:57:24 +0000

    Added DB connection to FastAPI

--- backend/python/main.py
+++ backend/python/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "FastAPI running in Docker"}
+# --- Database Connection ---
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_db_connection():
+    conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
+    return conn
+
+@app.get("/db-test/{product_id}")
+def db_test(product_id: str):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT id, title, current_price FROM products WHERE id=%s", (product_id,))
+        row = cur.fetchone()
+        cur.close()
+        conn.close()
+
+        if not row:
+            return {"status": "not_found"}
+        return {"status": "ok", "product": row}
+
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
