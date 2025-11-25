7371d11c083a242bb1d1af6ac247eb51a6b79370
SaiTeja020 <sakamsaiteja@gmail.com>
2025-11-25 17:11:04 +0000

    Added DB connection to Node

--- backend/node/server.js
+++ backend/node/server.js
@@ -1,10 +1,31 @@
-const express = require("express");
+import pkg from "pg";
+import express from "express";
+const { Pool } = pkg;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Node API running in Docker" });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-app.listen(5000, () => {
-  console.log("Node API running on port 5000");
+app.get("/db-test/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query(
+      "SELECT id, title, current_price FROM products WHERE id = $1",
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.json({ status: "not_found" });
+    }
+
+    res.json({ status: "ok", product: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.json({ status: "error", message: err.message });
+  }
 });
